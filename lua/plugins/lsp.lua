400cfb16d71529dd57de9578e7c07625767eea4e
Damon Kreft <damon.kreft@beautybay.com>
2023-08-04 00:58:57 +0100

    MORE

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,24 +1,8 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    -- other settings removed for brevity
-    dependencies = { "jose-elias-alvarez/typescript.nvim" },
     opts = {
-      ---@type lspconfig.options
-      servers = {
-        eslint = {},
-        tsserver = {
-          keys = {
-            { "<leader>co", "<cmd>TypescriptOrganizeImports<CR>", desc = "Organize Imports" },
-            { "<leader>cR", "<cmd>TypescriptRenameFile<CR>", desc = "Rename File" },
-          },
-          settings = {
-            completions = {
-              completeFunctionCalls = true,
-            },
-          },
-        },
-      },
+      servers = { eslint = {} },
       setup = {
         eslint = function()
           require("lazyvim.util").on_attach(function(client)
@@ -28,10 +12,6 @@ return {
               client.server_capabilities.documentFormattingProvider = false
             end
           end)
-        end,
-        tsserver = function(_, opts)
-          require("typescript").setup({ server = opts })
-          return true
         end,
       },
     },
