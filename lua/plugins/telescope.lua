e820e493ea087d504e3d6603d9a920acf25e9b7e
Damon Kreft <damon.kreft@beautybay.com>
2023-08-06 17:31:53 +0100

    LSP reference widths

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  opts = {
+    pickers = {
+      lsp_references = { fname_width = 75 },
+    },
+    defaults = {
+      layout_config = {
+        width = 0.9,
+      },
+    },
+  },
+}
