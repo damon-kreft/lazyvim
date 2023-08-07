0b834b21cb7365a91e13a8edd36e531f6377ec72
Damon Kreft <damon.kreft@beautybay.com>
2023-08-07 21:40:24 +0100

    Better TS

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,27 @@
+local Util = require("lazyvim.util")
+
 return {
   "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader>ss",
+      Util.telescope("lsp_document_symbols", {
+        symbols = {
+          "Class",
+          "Function",
+          "Method",
+          "Constructor",
+          "Interface",
+          "Module",
+          "Struct",
+          "Trait",
+          "Field",
+          "Constant",
+        },
+      }),
+      desc = "Goto Symbol",
+    },
+  },
   opts = {
     pickers = {
       lsp_references = { fname_width = 75 },
---
+++ lua/plugins/typescript.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        vim.list_extend(opts.ensure_installed, { "javascript", "typescript", "tsx" })
+      end
+    end,
+  },
+  {
+    "pmizio/typescript-tools.nvim",
+    dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+    opts = {},
+  },
+}
